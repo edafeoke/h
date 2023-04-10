ca8b2593146f1490b6d737212b0b6addba610ede
Oke Edafe Great <greatedafeoke@gmail.com>
2023-04-10 21:36:20 +0100

    Added procfile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-	res.send('hello world!')
+	res.send(`hello ${process.env.PORT}`)
 });
 
 app.listen(process.env.PORT,()=>console.log(`listening ${process.env.PORT}`));
